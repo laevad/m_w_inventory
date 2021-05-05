16381890cdd3e411f3779dfa75fe2a6491131f95
laevad <aldave.tabian@ustp.edu.ph>
2021-05-05 21:41:29 +0800

    middleware

--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,18 @@ use App\Http\Controllers\DashboardController;
 |
 */
 
+
 Route::post('/auth/check', [MainController::class,'check'])->name('auth.check');
-Route::get('/auth/logout', [DashboardController::class,'logout'])->name('auth.logout');
 
-Route::get('/', [MainController::class,'login'])->name('login');
-
-Route::get('/admin/dashboard', [DashboardController::class,'dashboard'])->name('admin.dashboard');
-Route::get('/admin/addproduct', [DashboardController::class,'add'])->name('admin.add');
-Route::get('/admin/editproduct/{id}', [DashboardController::class,'edit'])->name('admin.edit');
-Route::post('/admin/saveEditProduct/', [DashboardController::class,'saveEditProduct'])->name('admin.saveEditProduct');
-Route::get('/admin/updateproduct', [DashboardController::class,'update'])->name('admin.update');
-Route::get('/admin/viewproduct', [DashboardController::class,'view'])->name('admin.view');
-Route::post('/admin/add', [DashboardController::class, 'addProduct'])->name('admin.addProduct');
-Route::get('/admin/delete/{id}', [DashboardController::class, 'destroy'])->name('admin.destroy');
+Route::group(['middleware'=>['AuthCheck']], function(){
+    Route::get('/', [MainController::class,'login'])->name('login');
+    Route::get('/auth/logout', [DashboardController::class,'logout'])->name('auth.logout');
+    Route::get('/admin/dashboard', [DashboardController::class,'dashboard'])->name('admin.dashboard');
+    Route::get('/admin/addproduct', [DashboardController::class,'add'])->name('admin.add');
+    Route::get('/admin/editproduct/{id}', [DashboardController::class,'edit'])->name('admin.edit');
+    Route::post('/admin/saveEditProduct/', [DashboardController::class,'saveEditProduct'])->name('admin.saveEditProduct');
+    Route::get('/admin/updateproduct', [DashboardController::class,'update'])->name('admin.update');
+    Route::get('/admin/viewproduct', [DashboardController::class,'view'])->name('admin.view');
+    Route::post('/admin/add', [DashboardController::class, 'addProduct'])->name('admin.addProduct');
+    Route::get('/admin/delete/{id}', [DashboardController::class, 'destroy'])->name('admin.destroy');
+});
